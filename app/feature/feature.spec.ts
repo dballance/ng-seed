c5fddde2d39cd7dfdab4e1e8f42991ad6d3040f2
dballance <dballance@gmail.com>
2015-12-16 22:20:07 -0600

    First pass of testing

--- app/feature/feature.spec.ts
+++ app/feature/feature.spec.ts
@@ -1,3 +1,25 @@
-export function stuff(){
-	console.log(stuff)
-}
+import * as angular from "angular-mocks/angular";
+
+describe("feature", () => {
+	var $compile;
+	var $rootScope;
+	
+	beforeEach(angular.mock.module("app.feature"));
+	
+	describe("feature directive", () => {
+		
+		beforeEach( () => {
+		angular.mock.inject( (_$compile_, _$rootscope_) => {
+			$compile = _$compile_;
+			$rootScope = _$rootscope_;
+		});
+		
+		});
+		
+		it("should say something", () => {
+			var element = $compile('<feature-directive></feature-directive>')($rootScope);
+			expect(element).toContain("Feature Directive");
+		});
+		
+	});
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,18 @@
+var path = require("path");
+
 module.exports = function (config) {
 	config.set({
 
 		basePath: '',
 		frameworks: ['jasmine'],
 		files: [
-			{ patter: "./app/**/*.ts", watched: true },
-			{ pattern: "./app/**/*.spec.js", watched: false }
+			 { pattern: 'karma.tests.js', watched: false }
 		],
 		exclude: [
 
 		],
 		preprocessors: {
-			"./app/**/*.spec.js": ['webpack']
+			'karma.tests.js': ['webpack', 'sourcemap']
 		},
 		reporters: ["dots", "coverage"],
 		coverageReporter: {
@@ -28,24 +29,28 @@ module.exports = function (config) {
 		webpack: {
 			resolve: {
 				cache: false,
-				root: __dirname,
-
+				root: path.join(__dirname, 'node_modules'),
+				extensions: ['','.ts','.js','.json', '.css', '.html'],
+				modulesDirectories: ["node_modules"],
+				alias: {
+					"app": "app"
+				}
 			},
 			devtool: 'inline-source-map',
 			module: {
 				loaders: [
 					// Support for *.json files.
-					{ test: /\.json$/, loader: 'json' },
+					{ test: /\.json$/, loader: 'json-loader' },
 					// Support for CSS as raw text
-					{ test: /\.css$/, loader: 'raw' },
+					{ test: /\.css$/, loader: 'raw-loader' },
 
 					// support for .html as raw text
-					{ test: /\.html$/, loader: 'raw' },
+					{ test: /\.html$/, loader: 'raw-loader' },
 
 					// Support for .ts files.
 					{
 						test: /\.ts$/,
-						loader: 'ts',
+						loader: 'ts-loader',
 						query: { 'ignoreDiagnostics': [2403] }, // 2403 -> Subsequent variable declarations
 						exclude: [/\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/]
 					}
@@ -55,7 +60,7 @@ module.exports = function (config) {
 						test: /\.ts$/,
 						exclude: [
 							/node_modules\//,
-							/spec.js$/
+							/\.spec.js$/
 						],
 						loader: 'istanbul-instrumenter'
 					}
@@ -65,7 +70,8 @@ module.exports = function (config) {
 			debug: false,
 		},
 		webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    },
+			noInfo: true //please don't spam the console when running in karma!
+		},
+		noResolve: false
 	})
 }
